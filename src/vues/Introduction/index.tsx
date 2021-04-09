74d345527f96a0e01c34942d596a24419cc0bb52
Tanguy <tanguy.maraux@epita.fr>
2021-04-09 07:39:14 +0100

    speed up intro animation

--- src/vues/Introduction/index.tsx
+++ src/vues/Introduction/index.tsx
@@ -9,36 +9,36 @@ function Introduction() {
                     <div className={styles.text}>
                         <div className={styles.line}>
                             <div className={styles.word}>
-                                <Bounce cascade triggerOnce damping={0.1}>
+                                <Bounce cascade triggerOnce damping={0.05}>
                                     <h1>H</h1><h1>i</h1><h1>!</h1>
                                 </Bounce>
                             </div>
                         </div>
                         <div className={styles.line}>
                             <div className={styles.word}>
-                                <Bounce cascade triggerOnce damping={0.1} delay={800}>
+                                <Bounce cascade triggerOnce damping={0.05} delay={600}>
                                     <h1>I'</h1><h1>m</h1>
                                 </Bounce>
                             </div>
                             <div className={styles.word}>
-                                <Bounce cascade triggerOnce damping={0.1} delay={1000}>
+                                <Bounce cascade triggerOnce damping={0.05} delay={800}>
                                     <h2>T</h2><h2>a</h2><h2>n</h2><h2>g</h2><h2>u</h2><h2>y</h2>
                                 </Bounce>
                             </div>
                             <div className={styles.word}>
-                                <Bounce cascade triggerOnce damping={0.1} delay={1700}>
+                                <Bounce cascade triggerOnce damping={0.05} delay={1100}>
                                     <h2>M</h2><h2>a</h2><h2>r</h2><h2>a</h2><h2>u</h2><h2>x</h2><h1>,</h1>
                                 </Bounce>
                             </div>
                         </div>
                         <div className={styles.line}>
                             <div className={styles.word}>
-                                <Bounce cascade triggerOnce damping={0.1} delay={3000}>
+                                <Bounce cascade triggerOnce damping={0.05} delay={1800}>
                                     <h1>F</h1><h1>u</h1><h1>l</h1><h1>l</h1><h1>S</h1><h1>t</h1><h1>a</h1><h1>c</h1><h1>k</h1>
                                 </Bounce>
                             </div>
                             <div className={styles.word}>
-                                <Bounce cascade triggerOnce damping={0.1} delay={3900}>
+                                <Bounce cascade triggerOnce damping={0.05} delay={2300}>
                                     <h1>d</h1><h1>e</h1><h1>v</h1><h1>.</h1>
                                 </Bounce>                                    
                             </div>
