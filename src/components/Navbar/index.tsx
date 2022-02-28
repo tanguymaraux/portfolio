7d781921a45d34e76dacee01a6805bd6b7760fdc
Tanguy <tanguy.maraux@epita.fr>
2022-02-28 01:57:49 +0100

    fix(menu): fix delay display

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,31 +1,51 @@
-import useMediaQuery from '@material-ui/core/useMediaQuery';
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { useState } from "react";
 import { Fade } from "react-awesome-reveal";
-import menuicon from '../../assets/icons/menu-icon.svg';
-import styles from './navbar.module.scss';
- 
+import menuicon from "../../assets/icons/menu-icon.svg";
+import styles from "./navbar.module.scss";
+
 export default function Navbar() {
+  const matches = useMediaQuery("(min-width: 768px)");
 
-    const matches = useMediaQuery('(min-width: 768px)');
+  var [isActive, setActive] = useState<boolean>();
+  const handleMenu = () => setActive(!isActive);
+  if (matches && isActive) {
+    setActive(false);
+  }
 
-    var [isActive, setActive] = useState<boolean>()
-    const handleMenu = () => setActive(!isActive)
-    if (matches && isActive) {
-        setActive(false);
-    }
-
-    return (
-        <div className={styles.navbar}>
-           { matches||isActive ? 
-            <Fade cascade damping={0.2} triggerOnce delay={3000}>
-                <a href="#home" id="home_b">Home</a>
-                <a href="#about" id="about_b">About</a>
-                <a href="#technologies" id="tech_b">Technologies</a>
-                <a href="#github" id="projects_b">Projects</a>
-                <a href="#footer" id="contact_b">Contact</a>
-            </Fade> : ""
-            }
-            <img onClick={handleMenu} className={styles.icon} src={menuicon} alt="menuicon"/>
+  return (
+          <Fade cascade damping={0.2} triggerOnce delay={3000}>
+    <div className={styles.navbar}>
+      {matches || isActive ? (
+        <div className={styles.navbar_menu}>
+          <Fade cascade damping={0.1} triggerOnce>
+            <a href="#home" id="home_b">
+              Home
+            </a>
+            <a href="#about" id="about_b">
+              About
+            </a>
+            <a href="#technologies" id="tech_b">
+              Technologies
+            </a>
+            <a href="#github" id="projects_b">
+              Projects
+            </a>
+            <a href="#footer" id="contact_b">
+              Contact
+            </a>
+          </Fade>
         </div>
-    )
+      ) : (
+        ""
+      )}
+      <img
+        onClick={handleMenu}
+        className={styles.icon}
+        src={menuicon}
+        alt="menuicon"
+      />
+    </div>
+          </Fade>
+  );
 }
