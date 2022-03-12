6d939927f8e6ed8b2906b3f17babb831ac7fbe28
Tanguy <tanguy.maraux@epita.fr>
2022-03-12 18:33:21 +0100

    feat(resume): add resume

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -14,38 +14,45 @@ export default function Navbar() {
   }
 
   return (
-          <Fade cascade damping={0.2} triggerOnce delay={3000}>
-    <div className={styles.navbar}>
-      {matches || isActive ? (
-        <div className={styles.navbar_menu}>
-          <Fade cascade damping={0.1} triggerOnce>
-            <a href="#home" id="home_b">
-              Home
-            </a>
-            <a href="#about" id="about_b">
-              About
-            </a>
-            <a href="#technologies" id="tech_b">
-              Technologies
-            </a>
-            <a href="#github" id="projects_b">
-              Projects
-            </a>
-            <a href="#footer" id="contact_b">
-              Contact
-            </a>
-          </Fade>
-        </div>
-      ) : (
-        ""
-      )}
-      <img
-        onClick={handleMenu}
-        className={styles.icon}
-        src={menuicon}
-        alt="menuicon"
-      />
-    </div>
-          </Fade>
+    <Fade cascade damping={0.2} triggerOnce delay={3000}>
+      <div className={styles.navbar}>
+        {matches || isActive ? (
+          <div className={styles.navbar_menu}>
+            <Fade cascade damping={0.1} triggerOnce>
+              <a href="#home" id="home_b">
+                Home
+              </a>
+              <a href="#about" id="about_b">
+                About
+              </a>
+              <a href="#technologies" id="tech_b">
+                Technologies
+              </a>
+              <a href="#github" id="projects_b">
+                Projects
+              </a>
+              <a href="#footer" id="contact_b">
+                Contact
+              </a>
+              <a
+                href="https://files.tanguymaraux.com/api/public/dl/_tosO_Eh?inline=true"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Resume
+              </a>
+            </Fade>
+          </div>
+        ) : (
+          ""
+        )}
+        <img
+          onClick={handleMenu}
+          className={styles.icon}
+          src={menuicon}
+          alt="menuicon"
+        />
+      </div>
+    </Fade>
   );
 }
